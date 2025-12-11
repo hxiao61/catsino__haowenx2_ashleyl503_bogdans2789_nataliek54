2250dcd1e62f602c1ba1f9cc5df01d6d3d4d5b22
hxiao61 <hxia61@stuy.edu>
2025-12-10 22:46:00 -0500

    blj

--- static/js/blj.js
+++ static/js/blj.js
@@ -31,13 +31,13 @@ async function startGame(){
     playerCards.push(drawData.cards[3]);
 
     updateUI();
-    checkForBlackJack();
+    checkForBlackjack();
 }
 
 async function hit(){
     if (!gameActive) return;
 
-    let response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
+    let response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     let data = await response.json();
 
     playerCards.push(data.cards[0]);
@@ -59,7 +59,7 @@ async function stand(){
         let data = await response.json();
         dealerCards.push(data.cards[0]);
         dealerScore = calculateScore(dealerCards);
-        displayCard(data.card[0], 'dealer-hand');
+        displayCard(data.cards[0], 'dealer-hand');
     }
 
     updateDealerScoreUI();
@@ -76,25 +76,83 @@ async function stand(){
 }
 
 function calculateScore(cards){
+    let score = 0;
+    let aces = 0;
 
+    for(let i = 0; i < cards.length; i++){
+        let card = cards[i];
+
+        if(card.value == "ACE") {
+            score += 11;
+            aces++;
+        } else if (card.value == "KING" || card.value == "QUEEN" || card.value == "JACK"){
+            score += 10;
+        } else {
+            score += parseInt(card.value);
+        }
+    }
+
+    while (score > 21 && aces > 0){
+        score -= 10;
+        aces -= 1;
+    }
+
+    return score;
 }
 
 function updateUI(){
+    document.getElementById('dealer-hand').innerHTML = "";
+    document.getElementById('player-hand').innerHTML = "";
 
+    for(let i = 0; i < dealerCards.length; i++){
+        let img = document.createElement('img');
+        img.className = 'card-img';
+
+        if (gameActive && i == 0){
+            img.src = "https://www.deckofcardsapi.com/static/img/back.png";
+        } else {
+            img.src = dealerCards[i].image;
+        }
+        document.getElementById('dealer-hand').appendChild(img);
+    }
+
+    for (let card of playerCards) {
+        displayCard(card, 'player-hand');
+    }
+    document.getElementById('player-score').innerText = calculateScore(playerCards);
 }
 
 function displayCard(card, elementID){
-
+    let img = document.createElement('img');
+    img.src = card.image;
+    img.className = 'card-img';
+    document.getElementById(elementID).appendChild(img);
 }
 
 function updateDealerScoreUI(){
-
+    document.getElementById('dealer-score').innerText = calculateScore(dealerCards);
+    let dealerDiv = document.getElementById('dealer-hand');
+    dealerDiv.innerHTML = "";
+    for (let card of dealerCards) {
+        let img = document.createElement('img');
+        img.src = card.image;
+        img.className = 'card-img';
+        dealerDiv.appendChild(img);
+    }
 }
 
 function checkForBlackjack() {
-
+    let pScore = calculateScore(playerCards);
+    if (pScore == 21){
+        endGame("Blackjack! You win!");
+    }
 }
 
 function endGame(msg) {
-
+    gameActive = false;
+    document.getElementById('message').innerText = msg;
+    document.getElementById('hit-btn').disabled = true;
+    document.getElementById('stand-btn').disabled = true;
+    document.getElementById('start-btn').disabled = false;
+    updateDealerScoreUI();
 }
