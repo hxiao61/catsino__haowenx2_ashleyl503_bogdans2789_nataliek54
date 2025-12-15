e65abeca5f69a63fc0f9df04a3880814cb2f270f
BogdanSotnikov <bogdans2789@nycstudents.net>
2025-12-15 11:47:23 -0500

    Working on poker logic

--- static/js/poker.js
+++ static/js/poker.js
@@ -17,16 +17,36 @@ async function Setup()
   cards.push(drawData.cards[3]);
   cards.push(drawData.cards[4]);
 
-  document.getElementById('cds').appendChild(document.createTextNode(cards[0].code));
-  document.getElementById('cds').appendChild(document.createTextNode(cards[1].code));
-  document.getElementById('cds').appendChild(document.createTextNode(cards[2].code));
-  document.getElementById('cds').appendChild(document.createTextNode(cards[3].code));
+  document.getElementById('cds').appendChild(document.createTextNode(cards[0].code + " "));
+  document.getElementById('cds').appendChild(document.createTextNode(cards[1].code + " "));
+  document.getElementById('cds').appendChild(document.createTextNode(cards[2].code + " "));
+  document.getElementById('cds').appendChild(document.createTextNode(cards[3].code + " "));
   document.getElementById('cds').appendChild(document.createTextNode(cards[4].code));
-
+  bet = document.getElementById('betOUT').value;
 }
 
-function SetBet()
+function CheckHand()
 {
-  bet = document.getElementById('betIN').value;
-  document.getElementById('betOUT').innerText = bet;
+  let message = document.getElementById('msg');
+  let hasHand = 0;
+  let multiplier = 0;
+
+  let tempCards = cards;
+  for (let i = 0; i < cards.length; i++)
+  {
+    tempCards.splice(i, 1);
+    if (tempCards.includes(cards[i]))
+    {
+      hasHand = 1;
+      multiplier = 1;
+      tempCards.splice(indexOf(cards[i]), 1)
+        multiplier = 3;
+
+    }
+  }
+
+  if (!hasHand)
+  {
+    message.innerText="You lost >:)";
+  }
 }
