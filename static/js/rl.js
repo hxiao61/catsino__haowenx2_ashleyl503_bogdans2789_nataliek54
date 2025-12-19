317b4796cac89e796acdaeb31a2b2abf54e30c70
Ashley Li <ali64@stuy.edu>
2025-12-18 23:36:47 -0500

    made buttons, need functions

--- static/js/rl.js
+++ static/js/rl.js
@@ -44,18 +44,13 @@ async function updateBalance(increment, won) {
 function one() { //bet on 1 - 18
   let x = getRndInteger(0, 36);
   // console.log(x);
-  // console.log(1 <= x && x<= 18);
+  // console.log(1 <= x && x <= 18);
   if (1 <= x && x<= 18) {
     updateBalance(50, true);
   } else {
     updateBalance(-100, false);
   }
 }
-
-document.getElementById("one").addEventListener('click', () => {
-  one();
-});
-
 
 function two() { //bet on even
   let x = getRndInteger(0, 36);
@@ -67,10 +62,6 @@ function two() { //bet on even
     updateBalance(-100, false);
   }
 }
-
-document.getElementById("two").addEventListener('click', () => {
-  two();
-});
 
 function three() { //bet on red
   let x = getRndInteger(0, 36);
@@ -87,19 +78,37 @@ function three() { //bet on red
   }
 }
 
-document.getElementById("three").addEventListener('click', () => {
-  three();
-});
-
 function four() { //bet on black
   let x = getRndInteger(0, 36);
   // console.log(x);
-  // console.log(x == 3 || x == 9 || x == 12 || x == 18 || x == 21 || x == 27 || x == 30||
-  //             x == 36 || x == 5 || x == 14 || x == 23 || x == 32 || x == 1 || x == 7 ||
-  //             x == 16 || x == 19 || x == 25 || x == 34);
-  if (x == 6 || x == 9 || x == 12 || x == 18 || x == 21 || x == 27 || x == 30||
-      x == 36 || x == 5 || x == 14 || x == 23 || x == 32 || x == 1 || x == 7 ||
-      x == 16 || x == 19 || x == 25 || x == 34) {
+  // console.log(x == 6 || x == 15 || x == 24 || x == 33 || x == 2 || x == 8 || x == 11||
+  //     x == 17 || x == 20 || x == 26 || x == 29 || x == 35 || x == 4 || x == 10 ||
+  //     x == 13 || x == 22 || x == 28 || x == 31);
+  if (x == 6 || x == 15 || x == 24 || x == 33 || x == 2 || x == 8 || x == 11||
+      x == 17 || x == 20 || x == 26 || x == 29 || x == 35 || x == 4 || x == 10 ||
+      x == 13 || x == 22 || x == 28 || x == 31) {
+    updateBalance(50, true);
+  } else {
+    updateBalance(-100, false);
+  }
+}
+
+function five() { //bet on odd
+  let x = getRndInteger(0, 36);
+  // console.log(x);
+  // console.log(x % 2 != 0);
+  if (x % 2 != 0) {
+    updateBalance(50, true);
+  } else {
+    updateBalance(-100, false);
+  }
+}
+
+function six() { //bet on 19 - 36
+  let x = getRndInteger(0, 36);
+  // console.log(x);
+  // console.log(19 <= x && x <= 36);
+  if (19 <= x && x<= 36) {
     updateBalance(50, true);
   } else {
     updateBalance(-100, false);
